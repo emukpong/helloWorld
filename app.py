3c5bdbea970630bd4aeaf6d59da871cfe2addc99
ememukpong <eukpong@umd.edu>
2024-03-26 22:07:32 -0400

    Added multiple new templates: - favorite-course.html prints out the query parameters for a subject and code presented in the url - contact.html gets information from user on a form upon first entry and posts the users entries on another template once submitted. applied based.html as parent template - base.html is a parent template
    Updated:
    - style.css to include a style for h1
    - app.py to include routes for new templates favorite-course.html and contact.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -19,5 +19,22 @@ def about():
 def aboutcss():
     return render_template('about-css.html')
 
+@app.route('/favorite-course', methods=['GET', 'POST'])
+def favoritecourse():
+    print('Subject: ' + request.args.get('subject'))
+    print('Course Number: ' + request.args.get('course_number'))
+
+    return render_template('favorite-course.html')
+
+@app.route('/contact', methods={'GET', 'POST'})
+def contact():
+    if request.method == 'POST':
+        print('First name entered: ' + request.form.get('first_name' or 'N/A'))
+        print('Last name entered: ' + request.form.get('last_name' or 'N/A'))
+        print('Email: ' + request.form.get('email' or 'N/A'))
+        print('Additional Comments: ' + request.form.get('additional_comments' or 'N/A'))
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
 if __name__ == '__main__':
     app.run()
